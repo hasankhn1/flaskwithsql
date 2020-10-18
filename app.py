578d594a1aea93f84274ea8bc621bfcb848b4766
Hassan <h.karim@sunandsandsports.com>
2020-10-18 15:15:26 +0400

    added postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
@@ -8,7 +9,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.secret_key = 'jose'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///store.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
